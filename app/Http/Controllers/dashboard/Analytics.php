4eb6da067a29f84796d6ea29d893207b685cadb7
webpioneer23 <webpioneer23@gmail.com>
2024-01-16 13:59:41 +0200

    url config

--- app/Http/Controllers/dashboard/Analytics.php
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Models\Contract;
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+
 
 class Analytics extends Controller
 {
   public function index()
   {
-    return public_path("files/X3BYjItXk6KMCXKyQkbLaHkpBwZB5RrOUgpjGXBy.doc");
+    return URL::asset("files/X3BYjItXk6KMCXKyQkbLaHkpBwZB5RrOUgpjGXBy.doc");
     $listing_count = Listing::count();
     $listing_price = Listing::sum('price');
     $contracts_count = Contract::count();
